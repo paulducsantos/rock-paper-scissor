bd3143cbeb467fde61aa82e73cd7d8598cd42258
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-07 21:22:25 -0500

    alerts player if they won or lost and keeps track of score

--- js/game.js
+++ js/game.js
@@ -6,37 +6,41 @@ $(document).ready(function(){
   var computerWins = 0;
   var gameCount = 0;
 
-  $("[data-throw=rock]").on("click", function(){
+  $("button").on("click", function(){
+    gameCount++;
+    var playerHand = $(this).data("throw");
     var compSelect = hands[Math.floor(Math.random()*3)];
-    if (compSelect === "rock"){
+    
+    if (playerHand === compSelect){
+
       alert("tie game");
-    } else if (compSelect === "paper"){
+    } else if (playerHand === "rock" && compSelect === "paper"){
       alert("you lose");
-    } else if (compSelect === "scissors"){
+      computerWins++;
+    } else if (playerHand === "rock" && compSelect === "scissors"){
       alert("you win");
+      playerWins++;
+    } else if (playerHand === "paper" && compSelect === "rock"){
+      alert("you win");
+      playerWins++;
+    } else if (playerHand === "paper" && compSelect === "scissors"){
+      alert("you lose");
+      computerWins++;
+    } else if (playerHand === "scissors" && compSelect === "rock"){
+      alert("you lose");
+      computerWins++;
+    } else if (playerHand === "scissors" && compSelect === "paper"){
+      alert("you win");
+      playerWins++;
     }
+    console.log("you picked " + playerHand, "computer picked " + compSelect);
+    console.log("player wins " + playerWins, ", computerWins " + computerWins, ", games played " + gameCount);
   });
 
-  $("[data-throw=paper]").on("click", function(){
-    var compSelect = hands[Math.floor(Math.random()*3)];
-    if (compSelect === "rock"){
-      alert("you win");
-    } else if (compSelect === "paper"){
-      alert("tie game");
-    } else if (compSelect === "scissors"){
-      alert("you lose");
-    }
-  });
-
-  $("[data-throw=scissors]").on("click", function(){
-    var compSelect = hands[Math.floor(Math.random()*3)];
-    if (compSelect === "rock"){
-      alert("you lose");
-    } else if (compSelect === "paper"){
-      alert("you win");
-    } else if (compSelect === "scissors"){
-      alert("tie game");
-    }
-  });
+  if (gameCount === 5){
+    playerWins = 0;
+    computerWins = 0;
+    gameCount = 0;
+  }
 
 });
