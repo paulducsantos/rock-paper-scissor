4f8971915b34438941f2069f0a56fe7d0426bdb9
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-07 20:48:34 -0500

    wrote basic html and the hands available for game

--- js/game.js
+++ js/game.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
 
+  var hands = ["rock", "paper", "scissor"];
 
-  
-})
+
+  $("[data-throw=rock]").on("click", function(){
+    var compSelect = hands[Math.floor(Math.random()*2)];
+  });
+
+});
