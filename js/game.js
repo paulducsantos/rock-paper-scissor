d091c3552095fdc4c2ba18c3f212c55ae734e11d
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-07 21:00:18 -0500

    clicking on button will let player win or lose. Computer selection comes from random item from array

--- js/game.js
+++ js/game.js
@@ -1,11 +1,42 @@
 $(document).ready(function(){
 
 
-  var hands = ["rock", "paper", "scissor"];
-
+  var hands = ["rock", "paper", "scissors"];
+  var playerWins = 0;
+  var computerWins = 0;
+  var gameCount = 0;
 
   $("[data-throw=rock]").on("click", function(){
-    var compSelect = hands[Math.floor(Math.random()*2)];
+    var compSelect = hands[Math.floor(Math.random()*3)];
+    if (compSelect === "rock"){
+      alert("tie game");
+    } else if (compSelect === "paper"){
+      alert("you lose");
+    } else if (compSelect === "scissors"){
+      alert("you win");
+    }
+  });
+
+  $("[data-throw=paper]").on("click", function(){
+    var compSelect = hands[Math.floor(Math.random()*3)];
+    if (compSelect === "rock"){
+      alert("you win");
+    } else if (compSelect === "paper"){
+      alert("tie game");
+    } else if (compSelect === "scissors"){
+      alert("you lose");
+    }
+  });
+
+  $("[data-throw=scissors]").on("click", function(){
+    var compSelect = hands[Math.floor(Math.random()*3)];
+    if (compSelect === "rock"){
+      alert("you lose");
+    } else if (compSelect === "paper"){
+      alert("you win");
+    } else if (compSelect === "scissors"){
+      alert("tie game");
+    }
   });
 
 });
