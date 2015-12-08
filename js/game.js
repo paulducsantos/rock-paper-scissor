cf0c7f02efaf0315c810c45cfc4df7afcaa019d7
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-07 21:29:05 -0500

    count restarts after 5 games and prepping to change to case statement instead of if else

--- js/game.js
+++ js/game.js
@@ -7,40 +7,42 @@ $(document).ready(function(){
   var gameCount = 0;
 
   $("button").on("click", function(){
+    if (gameCount === 5){
+    playerWins = 0;
+    computerWins = 0;
+    gameCount = 0;
+    }
     gameCount++;
     var playerHand = $(this).data("throw");
     var compSelect = hands[Math.floor(Math.random()*3)];
     
-    if (playerHand === compSelect){
-
-      alert("tie game");
-    } else if (playerHand === "rock" && compSelect === "paper"){
-      alert("you lose");
-      computerWins++;
-    } else if (playerHand === "rock" && compSelect === "scissors"){
-      alert("you win");
-      playerWins++;
-    } else if (playerHand === "paper" && compSelect === "rock"){
-      alert("you win");
-      playerWins++;
-    } else if (playerHand === "paper" && compSelect === "scissors"){
-      alert("you lose");
-      computerWins++;
-    } else if (playerHand === "scissors" && compSelect === "rock"){
-      alert("you lose");
-      computerWins++;
-    } else if (playerHand === "scissors" && compSelect === "paper"){
-      alert("you win");
-      playerWins++;
-    }
+    // change to case statement instead
+    // if (playerHand === compSelect){
+    //   alert("tie game");
+    // } else if (playerHand === "rock" && compSelect === "paper"){
+    //   alert("you lose");
+    //   computerWins++;
+    // } else if (playerHand === "rock" && compSelect === "scissors"){
+    //   alert("you win");
+    //   playerWins++;
+    // } else if (playerHand === "paper" && compSelect === "rock"){
+    //   alert("you win");
+    //   playerWins++;
+    // } else if (playerHand === "paper" && compSelect === "scissors"){
+    //   alert("you lose");
+    //   computerWins++;
+    // } else if (playerHand === "scissors" && compSelect === "rock"){
+    //   alert("you lose");
+    //   computerWins++;
+    // } else if (playerHand === "scissors" && compSelect === "paper"){
+    //   alert("you win");
+    //   playerWins++;
+    // }
+    
     console.log("you picked " + playerHand, "computer picked " + compSelect);
     console.log("player wins " + playerWins, ", computerWins " + computerWins, ", games played " + gameCount);
   });
 
-  if (gameCount === 5){
-    playerWins = 0;
-    computerWins = 0;
-    gameCount = 0;
-  }
+  
 
 });
