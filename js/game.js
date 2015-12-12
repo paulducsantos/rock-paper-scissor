84c5d40a6525450fd503c1431387f27b22d79c36
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-12 00:09:15 -0500

    added player knockdown animation

--- js/game.js
+++ js/game.js
@@ -26,9 +26,13 @@ $(document).ready(function(){
       tieGame();
     } else if (playerHand === "rock" && compSelect === "paper"){
       console.log("you lose");
-      getHit();
-      computerWins++;
       playerHealth-= 25;
+      if (playerHealth === 0){
+        littleMacDown();
+      } else {
+        getHit();
+      }
+      computerWins++;
     } else if (playerHand === "rock" && compSelect === "scissors"){
       console.log("you win");
       attack();
@@ -39,14 +43,22 @@ $(document).ready(function(){
       playerWins++;
     } else if (playerHand === "paper" && compSelect === "scissors"){
       console.log("you lose");
-      getHit();
-      computerWins++;
       playerHealth-= 25;
+      if (playerHealth === 0){
+        littleMacDown();
+      } else {
+        getHit();
+      }
+      computerWins++;
     } else if (playerHand === "scissors" && compSelect === "rock"){
       console.log("you lose");
-      getHit();
-      computerWins++;
       playerHealth-= 25;
+      if (playerHealth === 0){
+        littleMacDown();
+      } else {
+        getHit();
+      }
+      computerWins++;
     } else if (playerHand === "scissors" && compSelect === "paper"){
       console.log("you win");
       attack();
@@ -63,12 +75,12 @@ $(document).ready(function(){
 
     function tieGame (){
         $(".little-mac-container").toggleClass("little-mac-animate little-mac-dodge-left");
-        $(".littleMac").toggleClass("littleMac dodge-left");
+        $(".littleMac-player").toggleClass("littleMac dodge-left");
         $(".tyson-animate").toggleClass("tyson-animate tyson-punch");
         $(".mikeTyson").toggleClass("mikeTyson tyson-left-upper");
         setTimeout(function(){
             $(".little-mac-dodge-left").toggleClass("little-mac-animate little-mac-dodge-left");
-            $(".dodge-left").toggleClass("littleMac dodge-left");
+            $(".littleMac-player").toggleClass("littleMac dodge-left");
             $(".tyson-punch").toggleClass("tyson-animate tyson-punch");
             $(".tyson-left-upper").toggleClass("mikeTyson tyson-left-upper");
         }, 1100)
@@ -76,12 +88,12 @@ $(document).ready(function(){
 
     function getHit(){
         $(".little-mac-container").toggleClass("little-mac-animate");
-        $(".littleMac").toggleClass("littleMac little-mac-hit");
+        $(".littleMac-player").toggleClass("littleMac little-mac-hit");
         $(".tyson-animate").toggleClass("tyson-animate tyson-punch");
         $(".mikeTyson").toggleClass("mikeTyson tyson-left-upper");
         setTimeout(function(){
             $(".little-mac-container").toggleClass("little-mac-animate");
-            $(".little-mac-hit").toggleClass("littleMac little-mac-hit");
+            $(".littleMac-player").toggleClass("littleMac little-mac-hit");
             $(".tyson-punch").toggleClass("tyson-animate tyson-punch");
             $(".tyson-left-upper").toggleClass("mikeTyson tyson-left-upper");
         }, 1100)
@@ -89,12 +101,25 @@ $(document).ready(function(){
 
     function attack(){
         $(".little-mac-container").toggleClass("little-mac-animate little-mac-attack");
-        $(".littleMac").toggleClass("littleMac little-mac-punch");
+        $(".littleMac-player").toggleClass("littleMac little-mac-punch");
         $(".mikeTyson").toggleClass("mikeTyson tyson-hit");
         setTimeout(function(){
             $(".little-mac-attack").toggleClass("little-mac-animate little-mac-attack");
             $(".little-mac-punch").toggleClass("littleMac little-mac-punch");
             $(".tyson-hit").toggleClass("mikeTyson tyson-hit");
+        }, 1100)
+    }
+
+    function littleMacDown(){
+        $(".little-mac-container").toggleClass("little-mac-animate little-mac-knockdown");
+        $(".littleMac-player").toggleClass("littleMac little-mac-down");
+        $(".tyson-animate").toggleClass("tyson-animate tyson-punch");
+        $(".mikeTyson").toggleClass("mikeTyson tyson-left-upper");
+        setTimeout(function(){
+            $(".little-mac-container").toggleClass("little-mac-animate little-mac-knockdown");
+            $(".littleMac-player").toggleClass("littleMac little-mac-down");
+            $(".tyson-punch").toggleClass("tyson-animate tyson-punch");
+            $(".tyson-left-upper").toggleClass("mikeTyson tyson-left-upper");
         }, 1100)
     }
 
